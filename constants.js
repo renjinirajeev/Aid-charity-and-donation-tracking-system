a745a64283ff9d4f1b7b5fb441f81b9ad1a4316c
Renjini Rajeev <71512100+renjinirajeev@users.noreply.github.com>
2023-11-16 13:21:11 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,10 @@
+import charityAbi from "./charity.json"
+import token from "./token.json"
+
+
+export const contractAbi = charityAbi
+export const contractAddress = "0x0960Da4459eFf74968487766dff6a544cB26FbD7"
+
+
+export const tokenAbi = token
+export const tokenAddress = '0x1816E30d0A21f27ea385EEA4d5F2f2D6A680B293'
